a42ad4204668d04f1fd8487d174d7e9416a4e42e
Fatimat Olanrewaju <fatimat_olanrewaju@yahoo.com>
2020-12-28 12:10:26 +0100

    add port details

--- server/app.js
+++ server/app.js
@@ -9,6 +9,16 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
+// default URL for website
+app.use('/', function(req,res){
+  res.sendFile(path.join(__dirname+'/public/index.html'));
+  //__dirname : It will resolve to your project folder.
+});
+const server = http.createServer(app);
+const port = 3000;
+server.listen(port);
+console.debug('Server listening on port ' + port);
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
